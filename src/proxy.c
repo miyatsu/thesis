807936f75cbcef1a9206629212e5d1fad499055c
miyatsu <miyatsu@qq.com>
2017-05-15 22:57:54 +0800

    Set a basic frame of low end proxy, edit from an example of libevent.

--- src/proxy.c
+++ src/proxy.c
@@ -18,6 +18,7 @@
 
 void read_cb(struct bufferevent * bev, void * ptr)
 {
+	printf("read_cb called.\n");
 	;
 }
 
@@ -33,14 +34,34 @@ void event_cb(struct bufferevent * bev, short events, void * ptr)
 
 void accept_cb(struct evconnlistener * listener, evutil_socket_t sock, struct sockaddr * addr, int len, void * ptr)
 {
-	struct bufferevent * bev = NULL;
-	bev = bufferevent_socket_new(g_base, sock, BEV_OPT_CLOSE_ON_FREE | BEV_OPT_DEFER_CALLBACKS);
-	if ( !bev )
+	struct bufferevent * bev_in = NULL;
+	struct bufferevent * bev_out = NULL;
+
+	bev_in = bufferevent_socket_new(g_base, sock, BEV_OPT_CLOSE_ON_FREE | BEV_OPT_DEFER_CALLBACKS);
+	if ( !bev_in )
 	{
 		LOG(LOG_LEVEL_FATAL, "Create bufferevent failed.");
 		exit(-1);
 	}
-	bufferevent_setcb(bev, read_cb, write_cb, event_cb, NULL);
+	bev_out = bufferevent_socket_new(g_base, -1, BEV_OPT_CLOSE_ON_FREE | BEV_OPT_DEFER_CALLBACKS);
+	if ( !bev_out )
+	{
+		LOG(LOG_LEVEL_FATAL, "Create bufferevent failed.");
+		bufferevent_free(bev_in);
+		exit(-1);
+	}
+	if ( bufferevent_socket_connect(bev_out, (struct sockaddr * )&g_remote_addr, sizeof(struct sockaddr_in)) < 0 )
+	{
+		/* Error */
+		exit(-1);
+	}
+
+	bufferevent_setcb(bev_in, read_cb, write_cb, event_cb, bev_out);
+	bufferevent_setcb(bev_out, read_cb, write_cb, event_cb, bev_in);
+
+	bufferevent_enable(bev_in, EV_READ | EV_WRITE);
+	bufferevent_enable(bev_out, EV_READ | EV_WRITE);
+
 	return ;
 }
 
